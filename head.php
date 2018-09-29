6e6f0229b60986e20f4a240eaca0f7cdf26663d9
Marcelo-Diament <marcelodiament@gmail.com>
2018-09-29 20:45:16 -0300

    atualização-01-20180929

--- head.php
+++ head.php
@@ -1,57 +1,33 @@
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <!-- SCRIPT INICIAIS -->
-  <link href="https://fonts.googleapis.com/css?family=Anton|Poiret+One|Raleway" rel="stylesheet">
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <script defer="defer" src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script defer="defer" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script defer="defer" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-  <link type="text/css" rel="stylesheet" href="style.css">
-  <!-- SCHEMA.ORG -->
-  <script type="application/ld+json">
-      {
-        "@context": "http://schema.org",
-        "@type": "Organization",
-        "url": "http://fardasguara.hospedagemdesites.ws",
-        "logo": "http://fardasguara.hospedagemdesites.ws/img/logo-fardas-guara.png",
-        "description": "Fardas, Aventais, Dólmãs, Aventais e Uniformes Sob Medida",
-        "additionalType": "http://www.productontology.org/doc/Uniform",
-        "telephone" : "+55-11-98221-8144",
-        "email" : "contato@fardasguara.com",
-        "name" : "Fardas Guará",
-        "alternateName" : "Fardas Guará | Fardas Sob Medida",
-        "address" : {
-          "@type" : "PostalAddress",
-          "streetAddress" : "Rua Harmonia, Vila Madalena",
-          "addressLocality" : "São Paulo",
-          "addressRegion" : "São Paulo"
-          // "postalCode" : "06454-050"
-        },
-        "sameAs" : [ "https://www.facebook.com/fardasguara", "https://www.instagram.com/fardasguara/" ],
-        "contactPoint": [{
-          "@type": "ContactPoint",
-          "telephone": "+55-11-98221-8144",
-          "contactType": "customer service"
-        }]
-      }
-    </script>
-  <!-- META DATA (SEO) / OG -->
-  <meta name="author" content="Marcelo Diament, Djament Comunicação">
-  <meta name="robots" content="index,follow">
-  <meta name="reply-to" content="contato@fardasguara.com.br">
-  <meta name="theme-color" content="#000000">
-  <meta property="og:site_name" content="Fardas Guará | Fardas Sob Medida" />
-  <meta property="og:title" content="Fardas Guará | Fardas Sob Medida" />
-  <meta property="og:description" content="Fardas, Aventais, Dólmãs, Aventais e Uniformes Sob Medida" />
-  <meta property="og:url" content="http://fardasguara.hospedagemdesites.ws" />
-  <meta property="og:locale" content="pt-BR" />
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content="http://fardasguara.hospedagemdesites.ws/img/logo-fardas-guara.png" />
-  <meta property="og:image:alt" content="Fardas Guará | Fardas Sob Medida" />
-  <meta property="og:image:url" content="http://fardasguara.hospedagemdesites.ws/img/logo-fardas-guara.png" />
-  <meta property="og:image:type" content="img/png" />
-  <!-- for Chrome on Android, multi-resolution icon of 196x196 -->
-  <meta name="mobile-web-app-capable" content="yes">
-  <link rel="shortcut icon" sizes="196x196" href="img/logo-fardas-guara.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon.png">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124883543-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124883543-1');
+</script>
+<meta charset=UTF-8><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=5"><meta http-equiv=Cache-control content=public><meta http-equiv=Content-Type content="text/html; charset=utf-8"><link rel=manifest href=/manifest.json><link href=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css rel=stylesheet integrity=sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO crossorigin=anonymous><link rel=stylesheet href=https://use.fontawesome.com/releases/v5.2.0/css/all.css integrity=sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ crossorigin=anonymous><link rel=stylesheet href=style.min.css><link href="https://fonts.googleapis.com/css?family=Anton|Poiret+One|Raleway" rel=stylesheet><script defer=defer defer=defer src=https://code.jquery.com/jquery-3.3.1.slim.min.js integrity=sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo crossorigin=anonymous></script><script defer=defer defer=defer src=https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js integrity=sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49 crossorigin=anonymous></script><script defer=defer defer=defer src=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js integrity=sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy crossorigin=anonymous></script><script type=application/ld+json>{
+"@context": "http://schema.org",
+"@type": "Organization",
+"url": "https://fardasguara.com",
+"logo": "https://fardasguara.com/img/logo-fardas-guara.png",
+"description": "Fardas, Aventais, Dólmãs, Aventais e Uniformes Sob Medida",
+"additionalType": "http://www.productontology.org/doc/Uniform",
+"telephone" : "+55-11-98221-8144",
+"email" : "contato@fardasguara.com",
+"name" : "Fardas Guará",
+"alternateName" : "Fardas Guará",
+"address" : {
+"@type" : "PostalAddress",
+"streetAddress" : "Rua Harmonia, Vila Madalena",
+"addressLocality" : "São Paulo",
+"addressRegion" : "São Paulo"
+},
+"sameAs" : [ "https://www.facebook.com/fardasguara", "https://www.instagram.com/fardasguara/" ],
+"contactPoint": [{
+"@type": "ContactPoint",
+"telephone": "+55-11-98221-8144",
+"contactType": "customer service"
+}]
+}</script><meta name=revisit-after content="7 days"><meta name=identifier-URL content=https://www.fardasguara.com><meta name=abstract content="Fardas, Uniformes, Dólmãs e Aventais Sob Medida"><meta name=author content="Marcelo Diament, Djament Comunicação"><meta name=robots content=index,follow><meta name=contact content=contato@fardasguara.com><meta name=reply-to content=contato@fardasguara.com><meta name=copyright content="Fardas Guará"><meta name=rating content=general><meta name=web_author content="Djament Comunicação"><meta name=theme-color content=#000000><meta property=og:site_name content="Fardas Guará"><meta property=og:title content="Fardas Guará"><meta property=og:description content="Fardas, Aventais, Dólmãs, Aventais e Uniformes Sob Medida"><meta property=og:url content=https://fardasguara.com><meta property=og:locale content=pt-BR><meta name=og:locality content="São Paulo"><meta name=og:region content=SP><meta name=og:country-name content=BR><meta property=og:type content=website><meta property=og:image content=https://fardasguara.com/img/logo-fardas-guara.png><meta property=og:image:alt content="Fardas Guará | Fardas Sob Medida"><meta property=og:image:url content=https://fardasguara.com/img/logo-fardas-guara.png><meta property=og:image:type content=img/png><meta name=mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-title content="Fardas Guará"><meta name=apple-mobile-web-app-status-bar-style content=black><link rel="shortcut icon" sizes=196x196 href=img/logo-fardas-guara.png><link rel=icon type=image/png sizes=96x96 href=img/favicon.png>
